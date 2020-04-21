b89d636139f43218d183983e539a5f20fedf253f
Alan Li <li.alan011@gmail.com>
2020-04-20 17:10:47 -0700

    Change the fetch request to / for index.js

--- frontend-apis/public/js/index.js
+++ frontend-apis/public/js/index.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', async () => {
 	// await the fetch call from the api backend
 	try {
-		const res = await fetch('http://localhost:8080/businesses/1');
+		const res = await fetch('http://localhost:8080');
 		return await res.json();
 	} catch (e) {
 		console.error(e);
