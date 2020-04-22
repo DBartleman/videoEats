deef57110a120fab7e43c5248540a820f6d51b26
Alan Li <li.alan011@gmail.com>
2020-04-22 11:02:23 -0700

    Write a review to business is functioning

--- frontend-apis/public/js/write-a-review.js
+++ frontend-apis/public/js/write-a-review.js
@@ -1,61 +1,79 @@
-const reviewForm = document.querySelector('.create-review');
-const businessSearch = document.getElementById('businessName');
+document.addEventListener('DOMContentLoaded', async () => {
+	const reviewForm = document.querySelector('.create-review');
+	// Not using the business Search at this time **4/22/2020**
+	// const businessSearch = document.getElementById('businessName');
 
-reviewForm.addEventListener('submit', async (e) => {
-	// prevents the default of the submit button
-	e.preventDefault();
+	// Declare vairables for url
+	const url = location.href;
+	const urlSplit = url.split('/');
+	const id = urlSplit[4];
 
-	// Declare varibles to store and be parsed as a post method for the buisness
-	const formData = new FormData(reviewForm);
-	const businessName = formData.get('businessName');
-	const reviewText = formData.get('reviewText');
-	// const bizRatings = formData.get('bizRatings');
-	const body = { businessName, reviewText };
+	reviewForm.addEventListener('submit', async (e) => {
+		// prevents the default of the submit button
+		e.preventDefault();
 
-	// TODO: Create the event listener keyup for the business name search. this is a stretch goal.
-
-	// Need to somehow get the id
-	try {
-		const res = await fetch('http://localhost:8080/businesses/:id/reviews', {
-			method: 'POST',
-			body: JSON.stringify(body),
-			headers: {
-				'Content-Type': 'application/json',
-				Authorization: `${localStorage.getItem('VIDEO_EATS_ACCESS_TOKEN')}`
+		// Declare varibles to store and be parsed as a post method for the buisness
+		const formData = new FormData(reviewForm);
+		// const businessName = formData.get('businessName');
+		const reviewText = formData.get('reviewText');
+		// const bizRatings = formData.get('bizRatings');
+		const body = {
+			review: {
+				businessId: id,
+				reviewText,
+				userId: 1,
+				typeId: 2,
+				upVoteCount: 0,
+				downVoteCount: 0,
+				businessRating: 4.5
 			}
-		});
-		if (!res.ok) {
-			throw res;
-		}
+		};
 
-		window.location.href = '/businesses/:id';
-	} catch (err) {
-		if (err.status === 401) {
-			window.location.href = '/log-in';
-		}
-		if (err.status >= 400 && err.status < 600) {
-			const errorJSON = await err.json();
-			const errorsContainer = document.querySelector('.errors-container');
-			let errorsHtml = [
-				`
-                <div class="alert alert-danger">
-                    Something went wrong. Please try again.
-                </div>
-            `
-			];
-			const { errors } = errorJSON;
-			if (errors && Array.isArray(errors)) {
-				errorsHtml = errors.map(
-					(message) => `
+		// TODO: Create the event listener keyup for the business name search. this is a stretch goal.
+
+		// Need to somehow get the id
+		try {
+			const res = await fetch(`http://localhost:8080/businesses/${id}/reviews`, {
+				method: 'POST',
+				body: JSON.stringify(body),
+				headers: {
+					'Content-Type': 'application/json',
+					Authorization: `${localStorage.getItem('VIDEO_EATS_ACCESS_TOKEN')}`
+				}
+			});
+			if (!res.ok) {
+				throw res;
+			}
+
+			window.location.href = `/businesses/${id}`;
+		} catch (err) {
+			if (err.status === 401) {
+				window.location.href = '/log-in';
+			}
+			if (err.status >= 400 && err.status < 600) {
+				const errorJSON = await err.json();
+				const errorsContainer = document.querySelector('.errors-container');
+				let errorsHtml = [
+					`
                     <div class="alert alert-danger">
-                        ${message.message}
+                        Something went wrong. Please try again.
                     </div>
-                    `
-				);
+                `
+				];
+				const { errors } = errorJSON;
+				if (errors && Array.isArray(errors)) {
+					errorsHtml = errors.map(
+						(message) => `
+                        <div class="alert alert-danger">
+                            ${message.message}
+                        </div>
+                        `
+					);
+				}
+				errorsContainer.innerHTML = errorsHtml.join('');
+			} else {
+				alert('Something went wrong. Please check your internet connection and try again!');
 			}
-			errorsContainer.innerHTML = errorsHtml.join('');
-		} else {
-			alert('Something went wrong. Please check your internet connection and try again!');
 		}
-	}
+	});
 });
