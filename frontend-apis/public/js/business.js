7419610ce67e8b78d959f2efc6bdcffb021a749b
Alan Li <li.alan011@gmail.com>
2020-04-22 16:05:07 -0700

    Create business rating for business page

--- frontend-apis/public/js/business.js
+++ frontend-apis/public/js/business.js
@@ -18,8 +18,25 @@ document.addEventListener('DOMContentLoaded', async () => {
 		const { reviews } = await res.json();
 
 		// Finds the average of the ratings
-		// const businessRatingAverage = reviews.forEach()
-		const stars = document.querySelectorAll('.star');
+		let ratingsArray = [];
+		reviews.forEach((review) => {
+			ratingsArray.push(parseInt(review.businessRating));
+		});
+		console.log(ratingsArray);
+		const sumRating = ratingsArray.reduce((a, b) => {
+			return a + b;
+		}, 0);
+		const averageRating = sumRating / ratingsArray.length;
+		console.log(averageRating);
+		const businessStars = document.querySelectorAll('.business-star');
+		businessStars.forEach((star, index) => {
+			if (averageRating > index) {
+				star.classList.add('rated');
+			} else {
+				star.classList.remove('rated');
+			}
+		});
+		document.querySelector('.stars-business').setAttribute('data-rating', averageRating);
 
 		// render the reviews in cards
 		const reviewSection = document.querySelector('.review-section');
@@ -28,14 +45,35 @@ document.addEventListener('DOMContentLoaded', async () => {
 			(review) => `
 			<div class="card mt-2" id="review-${review.id}">
 				<div class="card-body">
+					<div class="stars" data-rating="${review.businessRating}">
+						<span class="star"></span>
+						<span class="star"></span>
+						<span class="star"></span>
+						<span class="star"></span>
+						<span class="star"></span>
+					</div>
 					<p class="card-text">${review.reviewText}</p>
 					<p class="card-text">${review.User.userName}</p>
 					<p class="card-text">${review.createdAt.slice(5, 10) + '-' + review.createdAt.slice(0, 4)}</p>
 				</div>
 			</div>
-		`
+			`
 		);
 		reviewSection.innerHTML = reviewCardsHTML.join('');
+
+		// Individual ratings from reviews
+		reviews.forEach((review, index) => {
+			const stars = document.querySelectorAll('.star');
+			const rating = document.querySelector('.stars').getAttribute('data-rating');
+			stars.forEach((star, index) => {
+				if (rating > index) {
+					star.classList.add('rated');
+				} else {
+					star.classList.remove('rated');
+				}
+			});
+			document.querySelector('.stars').setAttribute('data-rating', rating);
+		});
 	} catch (err) {
 		console.error(err);
 	}
