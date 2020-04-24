331ab7b0d8961b9308b79b2ddcb494b8c946a1e7
Alan Li <li.alan011@gmail.com>
2020-04-24 09:39:55 -0700

    remove tag search functionality at this time.

--- frontend-apis/index.js
+++ frontend-apis/index.js
@@ -12,7 +12,6 @@ app.set('view engine', 'pug');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
 	res.locals.mapAPI = mapAPI;
-	console.log(mapAPI);
 	next();
 });
 
@@ -49,7 +48,7 @@ app.get('/businesses/:id/write-a-review', async (req, res) => {
 	}
 });
 
-app.get('/businesses/search/?=', (req, res) => {
+app.get('/businesses/search', (req, res) => {
 	res.render('search');
 });
 
